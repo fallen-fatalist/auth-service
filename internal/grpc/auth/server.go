8c84a7cb603557ca1421ae93ec289d7d21e462a9
Murat <m_kairbek@outlook.com>
2024-06-04 20:39:45 +0500

    handlers of gRPC server are done;

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -10,7 +10,26 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(
+		ctx context.Context,
+		userID int64,
+	) (bool, error)
+}
+
 type serverAPI struct {
+	auth Auth
 	authv1.UnimplementedAuthServer
 }
 
@@ -18,8 +37,8 @@ const (
 	emptyValue = 0
 )
 
-func Register(gRPC *grpc.Server) {
-	authv1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	authv1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(
@@ -38,22 +57,55 @@ func (s *serverAPI) Login(
 		return nil, status.Error(codes.InvalidArgument, "app_id is required")
 	}
 
-	// TODO: implement login via auth service
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
 
 	return &authv1.LoginResponse{
-		Token: "12345",
+		Token: token,
 	}, nil
 }
 func (s *serverAPI) Register(
 	ctx context.Context,
 	req *authv1.RegisterRequest,
 ) (*authv1.RegisterResponse, error) {
-	panic("implement me")
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &authv1.RegisterResponse{
+		UserId: userID,
+	}, nil
+
 }
 
 func (s *serverAPI) IsAdmin(
 	ctx context.Context,
 	req *authv1.IsAdminRequest,
 ) (*authv1.IsAdminResponse, error) {
-	panic("implement me")
+	if req.GetUserId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO: error handling
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &authv1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
 }
